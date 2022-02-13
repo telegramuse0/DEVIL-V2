aa6647de1d295ec2e4291f818b84ef06133c3ac6
DeadlyTom <95464168+telegramuse0@users.noreply.github.com>
2022-02-13 22:45:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -112,7 +112,7 @@ async def next_page(bot, query):
 async def advantage_spoll_choker(bot, query):
     _, user, movie_ = query.data.split('#')
     if int(user) != 0 and query.from_user.id != int(user):
-        return await query.answer("𝔸𝕪𝕚𝕤𝕙𝕖𝕣𝕚", show_alert=True)
+        return await query.answer("Ask For Your Own Movies or Series😉", show_alert=True)
     if movie_  == "close_spellcheck":
         return await query.message.delete()
     movies = SPELL_CHECK.get(query.message.reply_to_message.message_id)
