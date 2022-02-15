5fea87ceaac2b1979de49ceb0510dc9be4d9d380
DeadlyTom <95464168+telegramuse0@users.noreply.github.com>
2022-02-15 12:50:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -127,7 +127,7 @@ async def advantage_spoll_choker(bot, query):
             k = (movie, files, offset, total_results)
             await auto_filter(bot, query, k)
         else:
-            k = await query.message.edit('THE MOVIE IS NOT YET RELEASED OR ADDED TO DATABASE💌 \nIF YOU DO NOT RECEIVE THE MOVIE/SERIES EVEN AFTER ENTERING THE CORRECT NAME \nTHEN TYPE @ADMIN #REQUEST MOVIE/SERIES NAME+LANG+YEAR, \nINFORM ADMIN IN THIS FORMAT 👈 \nWe Will ⬆️ Upload Within 24 Hours ⏳')
+            k = await query.message.edit('IF YOU DO NOT RECEIVE THE MOVIE/SERIES EVEN AFTER ENTERING THE CORRECT NAME \nINFORM ADMIN IN THIS FORMAT TYPE \n👉@ADMIN #REQUEST MOVIE/SERIES NAME+LANG+YEAR,\n\nWe Will ⬆️ Upload Within 24 Hours ⏳')
             await asyncio.sleep(300)
             await k.delete()
 
